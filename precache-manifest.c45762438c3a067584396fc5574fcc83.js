b9cda4af38b3e23fec6c84b79970c5aa1dd114ac
Dwane Pennant <pennandw@corp.ads>
2020-04-16 08:10:56 -0400

    Updates

---
+++ precache-manifest.c45762438c3a067584396fc5574fcc83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf9a04882b487ee729651fe79dae17d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "81333bc8cc370560f9ba",
+    "url": "/monsters/static/css/main.7a627ed4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81333bc8cc370560f9ba",
+    "url": "/monsters/static/js/main.ba19de7b.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
